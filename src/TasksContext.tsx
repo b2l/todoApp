0573444f0cf1ca9b9e956f2829d081d8eab13fa0
Nicolas Medda <nicolas@ling.fr>
2023-06-27 18:29:18 +0200

    Use prepare for adding todo

--- src/TasksContext.tsx
+++ src/TasksContext.tsx
@@ -1,18 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, PrepareAction, createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = [
-  { id: 1, title: 'task one', completed: false },
-  { id: 2, title: 'task two', completed: false },
-  { id: 3, title: 'task three', completed: false },
-  { id: 4, title: 'task for', completed: true },
+  { id: nanoid(), title: 'task one', completed: false },
+  { id: nanoid(), title: 'task two', completed: false },
+  { id: nanoid(), title: 'task three', completed: false },
+  { id: nanoid(), title: 'task for', completed: true },
 ]
+
+interface Task {
+  id: string
+  title: string
+  completed: boolean
+}
 
 export const tasksSlice = createSlice({
   name: 'tasks',
   initialState,
   reducers: {
-    taskAdded: (state, action) => {
-      return action.payload
+    taskAdded: {
+      reducer(state, action: any) {
+        state.push(action.payload)
+      },
+      prepare: (title: any) => {
+        const id = nanoid()
+        return {
+          payload: {
+            title,
+            id,
+            completed: false,
+          },
+        }
+      },
     },
     taskDeleted: (state, action) => {
       return state.filter((task) => task !== action.payload)
@@ -31,6 +49,7 @@ export const tasksSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { taskAdded, taskCompleted, taskDeleted, taskUncompleted } = tasksSlice.actions
+export const { taskAdded, taskCompleted, taskDeleted, taskUncompleted } =
+  tasksSlice.actions
 
 export default tasksSlice.reducer
